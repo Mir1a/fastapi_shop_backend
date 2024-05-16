a548dd67d2b26ad76f71ec5373dd323c38082f60
Mir1a <miriamotuss@gmail.com>
2024-05-16 11:31:21 +0300

    add dockerfile/docker-compose

--- src/product/routers.py
+++ src/product/routers.py
@@ -17,7 +17,16 @@ router = APIRouter(
 @router.post("/item_create")
 async def add_item(new_item:  Annotated[schemas.ItemCreate, Depends()], session: AsyncSession = Depends(get_async_session)):
     async with session.begin():
-        db_item = models.Item(title=new_item.title, description=new_item.description, price=new_item.price, code=new_item.code, color=new_item.color, weight=new_item.weight, height=new_item.height, width=new_item.width, types=new_item.types, amount=new_item.amount)
+        db_item = models.Item(title=new_item.title,
+                              description=new_item.description,
+                              price=new_item.price,
+                              code=new_item.code,
+                              color=new_item.color,
+                              weight=new_item.weight,
+                              height=new_item.height,
+                              width=new_item.width,
+                              types=new_item.types,
+                              amount=new_item.amount)
         session.add(db_item)
     return db_item
 
--- src/user/routers.py
+++ src/user/routers.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated, List
+
+from sqlalchemy.orm import selectinload
 
 from src.user import schemas, models
 from src.database import get_async_session, async_sessionmaker
@@ -27,3 +30,11 @@ async def create_user(user: Annotated[schemas.UserCreate, Depends()], session: A
 
         session.add(db_user)
     return db_user
+
+@router.get("/users_all")
+async def get_items():
+    async with async_sessionmaker() as session:
+        query = select(models.User).options(selectinload(models.User.items))
+        result = await session.execute(query)
+        items = result.scalars().all()
+        return items
